5ff5d2c304a5330e82dd064ca6ec6792931f5c8b
Duvan Perez <duvan.dev05@gmail.com>
2022-07-20 22:03:42 -0500

    Se crean objetos en ajva y python, utilizando metodos, constructores y imprimiendo por consola

--- Java/Car.java
+++ Java/Car.java
@@ -1,6 +1,12 @@
 class Car {
+   //Caracteristicas de la clase
    Integer id;
    String license;
    String driver;
    Integer passenger; 
+
+   //Metodos de la clase 
+   void printDataCar(){
+      System.out.println("License: " + license + " Driver: " + driver);
+   }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,16 @@
+from car import Car
+
 if __name__ == "__main__":
     print("hola mundoo")
+    
+    car  = Car()
+    car.license = "FKD439"
+    car.driver = "Fernando Martinez"
+    car.passenger = 4
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "GFT840"
+    car2.driver = "Luis calarlca"
+    print(vars(car2))
+    
